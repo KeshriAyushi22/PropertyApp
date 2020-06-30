eb9ae44734c054275649d35ef5db5aa4004a9436
ayushi_keshri <ayushi.keshri@nearex.com>
2020-07-01 00:12:58 +0530

    display the details

--- src/components/PropertyDetail.js
+++ src/components/PropertyDetail.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react'
+import { Typography, Card, CardContent, CardActions, IconButton } from '@material-ui/core';
+import { Favorite } from '@material-ui/icons';
+import "../css/GridData.css"
 
 export default class propertyDetail extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            tile: this.props.tile
+        }
+    }
     render() {
+        const { tile } = this.state;
+        console.log(tile)
         return (
-            <div>
+            <React.Fragment>
+            <div style={{ display: "flex" }}>
+                <Typography variant="h4">
+                    {tile.price}
+                </Typography>
+                <CardActions disableSpacing>
+                    <IconButton aria-label="add to favorites">
+                        <Favorite />
+                    </IconButton>
+                </CardActions>
+                </div>
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p" style={{ width: "30%" }}>
+                        {tile.address}
+                    </Typography>
+                </CardContent>
                 
-            </div>
+                </React.Fragment>
         )
     }
 }
--- src/services/propertyList.js
+++ src/services/propertyList.js
@@ -3,24 +3,48 @@ export const tileData = [
         img: 'https://ca-times.brightspotcdn.com/dims4/default/0175a6e/2147483647/strip/true/crop/1080x607+0+0/resize/840x472!/quality/90/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F62%2Fb2%2F272fb3f14c38f7d8d538be5577d5%2Fla-fi-hotprop-lindsey-buckingham-brentwood-hou-003',
         title: 'Property 1',
         author: 'author',
-        id:1
+        id:1,
+        price:"$19,000",
+        address:"Main Street Street North Star, Alberta T0H2T0",
+        count:{
+            bedroom : 2,
+            bathroom : 1
+        }
     },
     {
         img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQWONWDD91wNFheg2bBHhn54JHZkQAxPxZvlQ&usqp=CAU',
         title: 'Property 2',
         author: 'author',
-        id:2
+        id:2,
+        price:"$19,500",
+        address:"Main Street Street North Star, Alberta T0H2T0",
+        count:{
+            bedroom : 3,
+            bathroom : 2
+        }
     },
     {
         img: 'https://bloximages.newyork1.vip.townnews.com/fredericknewspost.com/content/tncms/assets/v3/editorial/1/af/1af57833-200f-5e56-ab5a-6f651279851b/5d29032ff2a4d.image.jpg?resize=1200%2C800',
         title: 'Property 3',
         author: 'author',
-        id:3
+        id:3,
+        price:"$20,000",
+        address:"Main Street Street North Star, Alberta T0H2T0",
+        count:{
+            bedroom : 4,
+            bathroom : 3
+        }
     },
     {
         img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSlG73oAPz0r8DA7Rier-k8CUkqjLrc8wK78A&usqp=CAU',
         title: 'Property 4',
         author: 'author',
-        id:4
+        id:4,
+        price:"$19,000",
+        address:"Main Street Street North Star, Alberta T0H2T0",
+        count:{
+            bedroom : 2,
+            bathroom : 1
+        }
     },
 ];
